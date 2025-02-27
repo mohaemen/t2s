41474add75d1f9f29a4031677233db566e0f4a7b
Mohaemen Giali <gialim@delawareconsulting.com>
2025-02-27 16:35:25 +0000

    remove dist

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const path = require('path');
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname),
     },
     devServer: {
         static: {
